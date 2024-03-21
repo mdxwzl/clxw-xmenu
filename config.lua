507d178398e3685e4a093bb803d871f538452cb5
yiruzu <94981325+yiruzu@users.noreply.github.com>
2024-03-21 21:21:42 +0100

    Added Close Option

--- config.lua
+++ config.lua
@@ -1,19 +1,18 @@
 Config = {
     Keybind = 'X',
-
     AllowMovement = true,
-    DisableMouseClicks = true, -- prevents accidentally hitting or aiming at someone if allowMovement is enabled
-    DisableMouseMovement = true, -- prevents looking around while ui is open if allowMovement is enabled
+    DisableMouseClicks = true, -- prevents accidentally hitting or aiming at someone if AllowMovement is enabled
+    DisableMouseMovement = true, -- prevents looking around while ui is open if AllowMovement is enabled
     HoldToShow = true,
-
-    --- ICON SOURCE: https://icon-sets.iconify.design/
+    
     Items = {
         {
             label = "Event",
-            icon = "streamline:business-handshake-solid",
+            icon = "streamline:business-handshake-solid", -- ICON SOURCE: https://icon-sets.iconify.design/
             execute = function()
                 TriggerEvent('esx_ambulancejob:revive')
-            end
+            end,
+            close = true, -- close menu after execution
         },
         {
             label = "Submenu",
@@ -28,14 +27,16 @@ Config = {
                     icon = "streamline:business-handshake-solid",
                     execute = function()
                         TriggerEvent('esx_ambulancejob:revive')
-                    end
+                    end,
+                    close = true,
                 },
                 {
                     label = "Event",
                     icon = "streamline:business-handshake-solid",
                     execute = function()
                         TriggerEvent('esx_ambulancejob:revive')
-                    end
+                    end,
+                    close = true,
                 },
                 {
                     label = "Submenu in Submenu",
@@ -50,14 +51,16 @@ Config = {
                             icon = "streamline:business-handshake-solid",
                             execute = function()
                                 TriggerEvent('esx_ambulancejob:revive')
-                            end
+                            end,
+                            close = true,
                         },
                         {
                             label = "Event",
                             icon = "streamline:business-handshake-solid",
                             execute = function()
                                 TriggerEvent('esx_ambulancejob:revive')
-                            end
+                            end,
+                            close = true,
                         },
                     }
                 },
@@ -68,28 +71,32 @@ Config = {
             icon = "streamline:business-handshake-solid",
             execute = function()
                 TriggerEvent('esx_ambulancejob:revive')
-            end
+            end,
+            close = true,
         },
         {
             label = "Event",
             icon = "streamline:business-handshake-solid",
             execute = function()
                 TriggerEvent('esx_ambulancejob:revive')
-            end
+            end,
+            close = true,
         },
         {
             label = "Event",
             icon = "streamline:business-handshake-solid",
             execute = function()
                 TriggerEvent('esx_ambulancejob:revive')
-            end
+            end,
+            close = true,
         },
         {
             label = "Event",
             icon = "streamline:business-handshake-solid",
             execute = function()
                 TriggerEvent('esx_ambulancejob:revive')
-            end
+            end,
+            close = true,
         },
     }
 }
